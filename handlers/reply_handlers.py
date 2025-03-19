689f59296844cfdb157b8250f9ab5a99ff5a214d
whylineee <msavin901001@gmail.com>
2025-03-19 20:54:07 +0200

    1

--- handlers/reply_handlers.py
+++ handlers/reply_handlers.py
@@ -1,13 +1,14 @@
 from aiogram import types, Router
 from keyboards.inline_keyboards import get_inline_test
-from keyboards.reply_keyboards import get_quiz_keyboard, get_main_keyboard
+from keyboards.reply_keyboards import get_quiz_keyboard, get_main_keyboard, get_searchion_keyboard
+
 
 router = Router()
 
 
 @router.message(lambda message: message.text == "Меню")
 async def test_handler(message: types.Message):
-    await message.answer("""It's test message!""", reply_markup=get_inline_test())
+    await message.answer("""Головне меню""", reply_markup=get_searchion_keyboard())
 
 @router.message(lambda message: message.text == "Анкета")
 async def questionnaire_handler(message: types.Message):
--- keyboards/reply_keyboards.py
+++ keyboards/reply_keyboards.py
@@ -5,8 +5,8 @@ def get_main_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Меню")],
-            [KeyboardButton(text="Анкета"), KeyboardButton(text="Меню")],
-            [KeyboardButton(text="Меню")]
+            [KeyboardButton(text="Анкета"), KeyboardButton(text="Зв'язатись з нами")],
+            [KeyboardButton(text="Підтримка 24/7")]
         ],
         resize_keyboard=True
     )
@@ -16,8 +16,8 @@ def get_main_keyboard() -> ReplyKeyboardMarkup:
 def get_quiz_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="1")],
-            [KeyboardButton(text="2"), KeyboardButton(text="3")],
+            [KeyboardButton(text="1. Поміняти фото")],
+            [KeyboardButton(text="2. Поміняти текст"), KeyboardButton(text="3")],
             [KeyboardButton(text="4")],
             [KeyboardButton(text="Головне меню")]
         ],
@@ -25,3 +25,14 @@ def get_quiz_keyboard() -> ReplyKeyboardMarkup:
     )
     return keyboard
 
+def get_searchion_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="1. Поміasdfs")],
+            [KeyboardButton(text="2. Помінddяти текст"), KeyboardButton(text="3sdfs")],
+            [KeyboardButton(text="4sdf")],
+            [KeyboardButton(text="Головне меню")]
+        ],
+        resize_keyboard=True
+    )
+    return keyboard
