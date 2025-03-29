a663db7c0f13f4aa433e699d0623b04af18f5bf6
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-03-29 17:28:40 +0200

    add changes

---
+++ states/msg_text_formatter.py
@@ -0,0 +1,3 @@
+def success_changing_description(data, id):
+
+    return "Your description has been successfully changed!"
--- utils/users.py
+++ utils/users.py
@@ -1,5 +1,6 @@
 import json
 
+
 def add_new_employee(data):
     userDataItem = {
         "id": data.id,
@@ -34,14 +35,32 @@ def add_new_employee(data):
     with open("data/employee.json", "w") as f:
         json.dump(emp, f, indent=4)
 
-def edit_field_employee(data, field, value):
-    print(data, field, value)
-    # with open("data/employee.json", "r") as file:
-    #     emp = json.load(file)
-    #
-    # for user in emp:
-    #     if user["id"] == data.id:
-    #         user[field] = value
-    #
-    # with open("data/employee.json", "w") as f:
-    #     json.dump(emp, f, indent=4)
+
+def edit_field_employee(msg, field, value):
+    data = msg.from_user
+    msg_text = ""
+    with open("data/employee.json", "r") as file:
+        emp = json.load(file)
+
+    for user in emp:
+        if user["id"] == data.id:
+            user[field] = value["edit_desc"]
+            msg_text = (f"Your {field} was successfully updated! \n"
+                        f"\n\n{field.capitalize()}: {value['edit_desc']}\n"
+                        f"Name: {data.full_name}\n"
+                        f"Username: {data.username}\n"
+                        f"Language code: {data.language_code}\n"
+                        f"Status: {user['status']}\n"
+                        f"Role: {user['role']}\n"
+                        f"Profile image: {user['profile_img']}\n"
+                        f"Description: {user['description']}\n"
+                        f"Skills: {user['skills']}\n"
+                        f"Locations: {user['locations']}\n"
+                        f"Experience: {user['experience']}\n"
+                        f"Direction: {user['direction']}\n"
+                        f"Salary: {user['salary']}\n")
+
+    with open("data/employee.json", "w") as f:
+        json.dump(emp, f, indent=4)
+
+    return msg_text
